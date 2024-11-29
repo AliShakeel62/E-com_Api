705e4a8fe13e24f46ac1388d0e038237a39e6ae5
M.Ali Shakeel <alishakeel567567@gmail.com>
2024-11-29 16:45:51 +0500

    first commit

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const App = express();
 const apirouter = require("./router/apiroute")
 const mongoose = require("mongoose")
 const api = require("./Model/Apimodel")
+const cors = require('cors')
 require("dotenv").config()
 App.use(express.json())
+App.use(cors())
+
+
+
 App.use("/api",apirouter)
+
 
 mongoose.connect(process.env.URL, {
     useNewUrlParser: true,
